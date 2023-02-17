497b7cae2c84e497c4bc0d8c77cdc96a8fa1506e
shankars99 <shankars0899@gmail.com>
2023-02-17 17:00:47 -0500

    tests work

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,29 @@
-extern crate paillier;
-use paillier::*;
+mod key_manager;
+mod paillier_crypto;
+
+use key_manager::keys;
+use paillier_crypto::paillier_enc;
 
 // cargo +nightly run
 fn main() {
-    let (enc_key, dec_key) = Paillier::keypair().keys();
+    let (enc_key, dec_key) = key_manager::keys();
+    let cipher = paillier_enc(10, 15, 2, enc_key);
+
+    println!("Decrypted: {}", paillier_crypto::paillier_dec(cipher, dec_key));
 }
 
-fn paillier_enc(add1: u64, add2: u64, mul1: u64, enc_key: EncryptionKey) -> EncodedCiphertext<u64> {
-    let c1 = Paillier::encrypt(&enc_key, add1);
-    let c2 = Paillier::encrypt(&enc_key, add2);
-
-    let c = Paillier::add(&enc_key, &c1, &c2);
-
-    let d = Paillier::mul(&enc_key, &c, mul1);
-    d
-}
-
-fn paillier_dec(d: EncodedCiphertext<u64>, dec_key: DecryptionKey) -> u64 {
-    let m: u64 = Paillier::decrypt(&dec_key, &d);
-    m
-}
-
-// write a test
 #[cfg(test)]
 mod tests {
+
     use super::*;
 
-    fn init() -> (EncryptionKey, DecryptionKey, u64, u64, u64){
-        let (enc_key, dec_key) = Paillier::keypair().keys();
+    fn print_type_of<T>(_: &T) -> &'static str {
+        return std::any::type_name::<T>();
+    }
+
+    fn init() -> (EncryptionKey, DecryptionKey, u64, u64, u64) {
+        let (enc_key, dec_key) = key_manager::keys();
+
         let add1: u64 = 15;
         let add2: u64 = 22;
         let mul1: u64 = 10;
@@ -36,7 +32,15 @@ mod tests {
     }
 
     #[test]
-    fn test_paillier() {
+    fn key_import_type_check() {
+        let (enc_key, dec_key) = key_manager::keys();
+
+        assert_eq!(print_type_of(&enc_key) == "paillier::EncryptionKey", true);
+        assert_eq!(print_type_of(&dec_key) == "paillier::DecryptionKey", true);
+    }
+
+    #[test]
+    fn paillier_enc_dec() {
         let (enc_key, dec_key, add1, add2, mul1) = init();
 
         let cipher: EncodedCiphertext<u64> = paillier_enc(add1, add2, mul1, enc_key);
